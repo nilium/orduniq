d245f8e95ac6a01cda5774e6aaead654a4349fa0
Noel Cower <ncower@gmail.com>
2016-06-06 09:00:19 -0700

    Initial commit
    Very quick tool written to deduplicate lines of junk.

---
+++ uniq.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"flag"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("orduniq: ")
+	flag.Parse()
+
+	var inputs []io.Reader
+
+	argv := flag.Args()
+	if len(argv) == 0 {
+		inputs = []io.Reader{os.Stdin}
+	} else {
+		usedStdin := false
+		for _, p := range argv {
+			if p == "-" {
+				if usedStdin {
+					log.Panic("standard input specified more than once")
+				}
+				usedStdin = true
+
+				inputs = append(inputs, os.Stdin)
+				continue
+			}
+
+			fi, err := os.Open(p)
+			if err != nil {
+				panic(err)
+			}
+			defer fi.Close()
+
+			inputs = append(inputs, fi)
+		}
+	}
+
+	hashes := make(map[[sha1.Size]byte]struct{})
+	input := bufio.NewReader(io.MultiReader(inputs...))
+
+	bufout := bufio.NewWriter(os.Stdout)
+	defer func() {
+		ferr := bufout.Flush()
+		if ferr != nil {
+			log.Panic("unable to flush output buffer: ", ferr)
+		}
+	}()
+
+	for {
+		line, err := input.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF && len(line) == 0 {
+				// Done
+				return
+			} else if err != io.EOF {
+				log.Panic("error reading input line: ", err)
+			}
+		}
+
+		data := line
+		if err != nil {
+			data = append(data, '\n')
+		}
+		sum := sha1.Sum(data)
+
+		if _, ok := hashes[sum]; ok {
+			continue
+		}
+		hashes[sum] = struct{}{}
+
+		bufout.Write(data)
+	}
+}
